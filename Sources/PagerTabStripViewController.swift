de690d8a473560e2684f246e3bc0f0b8e7fc4139
val <valentin.ftp@gmail.com>
2022-09-28 22:11:19 +0300

    fix

--- Sources/PagerTabStripViewController.swift
+++ Sources/PagerTabStripViewController.swift
@@ -407,6 +407,6 @@ open class PagerTabStripViewController: UIViewController, UIScrollViewDelegate {
     private var pageBeforeRotate = 0
     private var lastSize = CGSize(width: 0, height: 0)
     internal var isViewRotating = false
-    internal var isViewAppearing = false
+    public var isViewAppearing = false
 
 }
